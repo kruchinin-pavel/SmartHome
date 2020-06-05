ab98b3f93382c72d65ca103f3cdb94cb3b1272ac
kruchinin-pavel <kruchinin.1977@ya.ru>
2020-06-05 08:58:01 +0300

    Last working version

--- digispark_vlazhnost_zemli/digispark_vlazhnost_zemli.ino
+++ digispark_vlazhnost_zemli/digispark_vlazhnost_zemli.ino
@@ -1,7 +1,7 @@
 #define LED_PIN 1
 // 2-4
 #define RHEOSTAT_PIN 2
-#define SENS_PIN A0
+#define SENS_PIN 3
 #define PUMP_PIN 4
 
 #include "digispark.h"
@@ -18,72 +18,21 @@ void setup()
   pinMode(PUMP_PIN, OUTPUT);
 }
 
-int nextConfCheck = 0;
-int nextPumpCheck = 0;
-int nextDrynessCheck = 0;
-int nextBlink = 0;
-
-double lastDrynessLevel = 0.;
-
-void fetchPump() {
-  nextPumpCheck = localTime + 2 * 60 * 1000;
-}
-
-
-void fetchBlink() {
-  nextBlink = localTime + 1000;
-}
-
-void fetchDryness() {
-  nextDrynessCheck = localTime + 5000;
-  fetchBlink();
-}
-
-
-void fetchConf() {
-  nextConfCheck = localTime + 1000;
-  fetchPump();
-  fetchDryness();
-}
-
-long lastLocalTime = 100;
+int nextPump = 0;
 
 void loop() {
-  if (lastLocalTime > localTime) {
-    fetchConf();
-  }
-  double drynessLevel = getDrynessLevel(RHEOSTAT_PIN);
-  if (abs(drynessLevel - lastDrynessLevel) > 10) {
-    digitalWrite(LED_PIN, HIGH);
-    fetchConf();
-  }
-
-  if (nextConfCheck > 0) {
-    if (localTime > nextConfCheck) {
-      blink(100, 100, 3);
-      for (int i = 0; i < 3; i++) blinkPin(LED_PIN, 100, 100);
-      nextConfCheck = 0;
-      fetchDryness();
+  double drynessLevel = analogThrustedRead(RHEOSTAT_PIN);
+  double dryness = analogThrustedRead(SENS_PIN);
+  if (dryness < drynessLevel) {
+    blink(100, 100, 3);
+    nextPump = 30;
+    delay(500);
+  } else {
+    if (nextPump-- <= 0) {
+      blinkPin(PUMP_PIN, 3000, 0);
+      nextPump = 30*60;
     }
-  } else if (localTime > nextDrynessCheck) {
-    fetchDryness();
-    double dryness = analogThrustedRead(SENS_PIN);
-    //    showVal(dryness);
-    if (needPump(SENS_PIN, drynessLevel)) {
-      if (localTime > nextPumpCheck) {
-        pumpWater(PUMP_PIN, 3);
-        fetchPump();
-      } else {
-        pumpWater(LED_PIN, 3);
-      }
-    } else {
-      delay_(1000);
-    }
-  } else if (localTime > nextBlink) {
-    fetchBlink();
-    blink();
+    blink(1000, 100, 1);
   }
-  delay_(100);
-  lastDrynessLevel = drynessLevel;
-  lastLocalTime = localTime;
+  
 }
--- digispark_vlazhnost_zemli/func.h
+++ digispark_vlazhnost_zemli/func.h
@@ -1,85 +1,25 @@
 #ifndef func_h
 #define func_h 1
-long localTime = 0;
-
-void delay_(int ms){
-  localTime += ms;
-  delay(ms);
-  if (localTime> 10000000) localTime = 0;
-}
 
 void blinkPin(int pin, int highMs, int lowMs) {
   digitalWrite(pin, HIGH);
-  delay_(highMs);
+  delay(highMs);
   digitalWrite(pin, LOW);
-  delay_(lowMs);
+  delay(lowMs);
 }
 
 void blink(int highMs, int lowMs, int count) {
-  for(int i=0;i<count;i++) blinkPin(LED_PIN, highMs, lowMs);
+  for (int i = 0; i < count; i++) blinkPin(LED_PIN, highMs, lowMs);
 }
-void blink() {
-  blinkPin(LED_PIN, 100, 200);
-}
-
-void showVal(double value) {
-#ifdef ARDUINO
-  Serial.println(value);
-#endif
-  for (int i = 0; i < value / 50; i++) {
-    blink();
-  }
-}
-
-void pumpWater(int pumpPin, int durationSeconds) {
-  digitalWrite(pumpPin, HIGH);
-  digitalWrite(LED_PIN, HIGH);
-  delay_(durationSeconds * 1000);
-  digitalWrite(pumpPin, LOW);
-  digitalWrite(LED_PIN, LOW);
-}
-
 
 double analogThrustedRead(int pin) {
-  int count = 10;
+  int count = 50;
   double value = 0;
   for (int i = 0; i < count; i++) {
     value += analogRead_(pin);
-    delay_(10);
+    delay(10);
   }
   return value / count;
-}
-
-
-boolean needPump(int sensorPin, double drynessLevel) {
-  double dryness = analogThrustedRead(sensorPin);
-  showVal(dryness);
-  if (dryness > drynessLevel) {
-#ifdef ARDUINO
-    Serial.print("(pump)dryness : ");
-    Serial.print(dryness);
-    Serial.print(" > drynessLevel: ");
-    Serial.println(drynessLevel);
-#endif
-    return true;
-#ifdef ARDUINO
-  } else {
-    Serial.print("(nothing do)dryness : ");
-    Serial.print(dryness);
-    Serial.print(" < drynessLevel: ");
-    Serial.println(drynessLevel);
-#endif
-  }
-  return false;
-}
-
-double getDrynessLevel(int pin) {
-  double drynessLevel = analogThrustedRead(pin);
-#ifdef ARDUINO
-  Serial.print("Rheo: ");
-  Serial.println(drynessLevel);
-#endif
-  return drynessLevel;
 }
 
 #endif
