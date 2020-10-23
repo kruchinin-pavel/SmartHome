1269eca0a627303886a781ab373ab24eba1e2b9b
kruchinin-pavel <kruchinin.1977@gmail.com>
2020-10-23 20:21:32 +0300

    works

--- digispark_vlazhnost_zemli/digispark_vlazhnost_zemli.ino
+++ digispark_vlazhnost_zemli/digispark_vlazhnost_zemli.ino
@@ -3,127 +3,10 @@
 #include "arduino.h"
 #include "func.h"
 
-class NormalizedAnalogVal {
-    int pin;
-    String pinStr, toStringVal;
-    double sens = -1, sensMax = 600, sensMin = 400;
-  public:
-    NormalizedAnalogVal() {}
-    
-    NormalizedAnalogVal(int pin, String pinStr, double maxVal, double minVal) {
-      this->pin = pin;
-      this->pinStr = pinStr;
-      this->sensMax = maxVal;
-      this->sensMin = minVal;
-    };
-
-    double getVal() {
-      return sens;
-    }
-
-    void readVal() {
-      if (enabled()) {
-        sens = nomalize(analogThrustedRead(pin));
-      }
-    };
-
-    boolean enabled() {
-      return pin > 0;
-    }
-
-    void init() {
-      pinMode(pin, INPUT);
-      Serial.print(pinStr);
-      Serial.println(" made INPUT");
-    }
-
-    String toString() {
-      toStringVal = pinStr;
-      toStringVal += "=";
-      toStringVal += sens;
-      return toStringVal;
-    }
-
-  private:
-    double nomalize(double sens) {
-      if (sens > 0) {
-        if (sens < sensMin) sensMin = sens;
-        if (sens > sensMax) sensMax = sens;
-      }
-      if (sens > sensMax ||  sens < sensMin)  return -1;
-      return (sens - sensMin) * 1024 / (sensMax - sensMin + 1);
-    };
-};
-
-class Dispenser {
-    NormalizedAnalogVal  rheo;
-    NormalizedAnalogVal  sens;
-    int id, nextPump, pumpPin, ledPin;
-    String res;
-    String v;
-  public:
-    Dispenser(int id, int rheoPin, int sensPin, String sensPinStr, int pumpPin, int ledPin) {
-      this->id = id;
-      sens = NormalizedAnalogVal(sensPin, sensPinStr, 700., 400.);
-      rheo = NormalizedAnalogVal(rheoPin, "", 1024, 0);
-      this->pumpPin = pumpPin;
-      this->ledPin = ledPin;
-    };
-
-    void init() {
-      rheo.init();
-      sens.init();
-      pinMode(pumpPin, OUTPUT);
-      pinMode(ledPin, OUTPUT);
-    }
-
-    void update() {
-      rheo.readVal();
-      sens.readVal();
-      if (sens.getVal() > rheo.getVal()) {
-        digitalWrite(ledPin, HIGH);
-        if (nextPump-- <= 0) {
-          //          blinkPin(pumpPin, 3000, 0);
-          nextPump = 30 * 60;
-          v = " pmp ";
-        } else {
-          v = " w";
-          v += nextPump;
-        }
-        v += " ";
-      } else {
-        blinkPin(ledPin, 10, 0);
-        nextPump = 30;
-        v = " oki ";
-      }
-      //      Serial.println(toString());
-    };
-
-    String toString() {
-      String res = "";
-      res += id;
-      res += " s" + sens.toString();
-      if (rheo.enabled()) {
-        res += v;
-        res += "r";
-        res += rheo.toString();
-      }
-      res += ";\t";
-      this->res = res;
-      return this->res;
-    }
-
-  private:
-    void blink(int highMs, int lowMs, int count) {
-      for (int i = 0; i < count; i++) blinkPin(ledPin, highMs, lowMs);
-    }
-
-};
-
 Dispenser dispensers[] = {
-  Dispenser( 1, -1, A3, "A3", 5, 2),
-  Dispenser( 2, -1, A5, "A5", 6, 3),
-  Dispenser( 3, -1, A7, "A7", 7, 4)
+  Dispenser( 1, A0, "A0", A3, "A3", 5, 2),
+  Dispenser( 2, A1, "A1", A4, "A4", 6, 3),
+  Dispenser( 3, A2, "A2", A5, "A5", 7, 4)
 };
 size_t n;
 
@@ -138,10 +21,11 @@ void setup()
 
 void loop() {
   size_t n = sizeof(dispensers) / sizeof(dispensers[0]);
+  int totDelay = 0;
   for (int i = 0; i < n; i++) {
-    dispensers[i].update();
+    totDelay += dispensers[i].update();
   }
-  delay(1000);
+  if (1000 > totDelay) delay(1000 - totDelay);
 #ifdef ARDUINO
   for (int i = 0; i < n; i++) {
     Serial.print(dispensers[i].toString());
--- digispark_vlazhnost_zemli/func.h
+++ digispark_vlazhnost_zemli/func.h
@@ -1,11 +1,12 @@
 #ifndef func_h
 #define func_h 1
 
-void blinkPin(int pin, int highMs, int lowMs) {
+int blinkPin(int pin, int highMs, int lowMs) {
   digitalWrite(pin, HIGH);
   delay(highMs);
   digitalWrite(pin, LOW);
   if (lowMs > 0)   delay(lowMs);
+  return highMs + lowMs;
 }
 
 double analogThrustedRead(int pin) {
@@ -15,7 +16,128 @@ double analogThrustedRead(int pin) {
     value += analogRead_(pin);
     delay(10);
   }
-  return value / count;
+  return (int)(value / count);
 }
+
+
+class NormalizedAnalogVal {
+    int pin;
+    String pinStr, toStringVal;
+    double sens = -1, sensMax = 600, sensMin = 400;
+  public:
+    NormalizedAnalogVal() {}
+
+    NormalizedAnalogVal(int pin, String pinStr, double maxVal, double minVal) {
+      this->pin = pin;
+      this->pinStr = pinStr;
+      this->sensMax = maxVal;
+      this->sensMin = minVal;
+    };
+
+    double getVal() {
+      return sens;
+    }
+
+    void readVal() {
+      if (enabled()) {
+        sens = nomalize(analogThrustedRead(pin));
+      }
+    };
+
+    boolean enabled() {
+      return pin > 0;
+    }
+
+    void init() {
+      pinMode(pin, INPUT);
+      Serial.print(pinStr);
+      Serial.println(" made INPUT");
+    }
+
+    String toString() {
+      toStringVal = pinStr;
+      toStringVal += "=";
+      toStringVal += sens;
+      return toStringVal;
+    }
+
+  private:
+    int nomalize(double sens) {
+      if (sens > 0) {
+        if (sens < sensMin) sensMin = sens;
+        if (sens > sensMax) sensMax = sens;
+      }
+      if (sens > sensMax ||  sens < sensMin)  return -1;
+      return (sens - sensMin) * 1024 / (sensMax - sensMin + 1);
+    };
+};
+
+class Dispenser {
+    NormalizedAnalogVal  rheo;
+    NormalizedAnalogVal  sens;
+    int id, nextPump, pumpPin, ledPin;
+    String res;
+    String v;
+  public:
+    Dispenser(int id, int rheoPin, String rheoPinStr, int sensPin, String sensPinStr, int pumpPin, int ledPin) {
+      this->id = id;
+      sens = NormalizedAnalogVal(sensPin, sensPinStr, 700., 400.);
+      rheo = NormalizedAnalogVal(rheoPin, rheoPinStr, 1024, 0);
+      this->pumpPin = pumpPin;
+      this->ledPin = ledPin;
+    };
+
+    void init() {
+      rheo.init();
+      sens.init();
+      pinMode(pumpPin, OUTPUT);
+      pinMode(ledPin, OUTPUT);
+    }
+
+    int update() {
+      rheo.readVal();
+      sens.readVal();
+      int ret = 0;
+      if (sens.getVal() > rheo.getVal()) {
+        ret = blinkPin(ledPin, 1000, 0);
+        if (nextPump-- <= 0) {
+          ret += blinkPin(pumpPin, 3000, 0);
+          nextPump = 30 * 60;
+          v = " pmp ";
+        } else {
+          v = " w";
+          v += nextPump;
+        }
+        v += " ";
+      } else {
+        ret = blinkPin(ledPin, 10, 0);
+        nextPump = 30;
+        v = " oki ";
+      }
+      //      Serial.println(toString());
+      return ret;
+    };
+
+    String toString() {
+      String res = "";
+      res += id;
+      res += " s" + sens.toString();
+      if (rheo.enabled()) {
+        res += v;
+        res += "r";
+        res += rheo.toString();
+      }
+      res += ";\t";
+      this->res = res;
+      return this->res;
+    }
+
+  private:
+    void blink(int highMs, int lowMs, int count) {
+      for (int i = 0; i < count; i++) blinkPin(ledPin, highMs, lowMs);
+    }
+
+};
+
 
 #endif
